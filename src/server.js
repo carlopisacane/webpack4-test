91c184fc5037a9a8e6b878a0ca8b0cb967a1cdbd
carlopisacane <doncarlopisacane@gmail.com>
2020-03-02 00:17:29 +0100

    Serving HTML pages via Express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', (req, res) => {
-	res.send('Some dummy content')
+	const pathToHtmlFile = path.resolve(__dirname, '../dist/hello-world.html')
+	const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8')
+	res.send(contentFromHtmlFile)
 })
 
 app.listen(3000, () => {
