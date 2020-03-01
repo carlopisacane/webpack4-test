dbc5c5611239315cdf3d6fab4909246a52d01985
carlopisacane <doncarlopisacane@gmail.com>
2020-03-01 23:32:58 +0100

    Setting custom options for code splitting

--- src/chrono.js
+++ src/chrono.js
@@ -1,8 +1,8 @@
 import Heading from './components/heading/heading'
 import ChronoImage from './components/chrono-image/chrono-image'
-import _ from 'lodash'
+import React from 'react';
 
 const heading = new Heading()
-heading.render(_.upperFirst('chrono'))
+heading.render('chrono')
 const chronoImage = new ChronoImage()
 chronoImage.render()
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -16,7 +16,9 @@ module.exports = {
 	mode: 'production',
 	optimization: {
 		splitChunks: {
-			chunks: 'all'
+			chunks: 'all',
+			minSize: 7000,
+			automaticNameDelimiter: '_'
 		}
 	},
 	module: {
