2251b367f06980a29a6c4976384e47a50b4d582a
carlopisacane <doncarlopisacane@gmail.com>
2020-03-01 20:36:40 +0100

    Faster development with webpack dev server

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
