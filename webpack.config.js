d0eabab48c77e68207a1d6f15701744785679477
carlopisacane <doncarlopisacane@gmail.com>
2020-03-01 13:06:46 +0100

    Handling css with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/'
+		path: path.resolve(__dirname, './dist'),
+		publicPath: 'dist/'
 	},
 	mode: 'none',
 	module: {
@@ -13,6 +13,10 @@ module.exports = {
 			{
 				test: /\.(png|jpg)$/,
 				use: [ 'file-loader' ]
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
 			}
 		]
 	}
