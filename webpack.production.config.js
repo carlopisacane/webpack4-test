d29bbc1ece42d2c90d19b767bd549c8abdb8990a
carlopisacane <doncarlopisacane@gmail.com>
2020-03-02 11:28:19 +0100

    Importing CSS from Bootstrap

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -33,7 +33,19 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: function() {
+								return [ require('precss'), require('autoprefixer') ]
+							}
+						}
+					},
+					'sass-loader'
+				]
 			},
 			{
 				test: /\.js$/,
